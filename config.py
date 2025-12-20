31d376da3021eb0d99873e9b06418002ad6b6698
mr.solo dolo <rrothang@gmail.com>
2025-12-20 18:53:33 +0200

    updated config by adding name

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
+
 # In your config.py
 MODEL_PATH = 'yolo11n.pt'  # Use the Nano version for speed and efficiency,YOLO, auto-downloads
 CONFIDENCE = 0.5  # Lower for maximum detections
-=======
-#  Real-time optimized settings
-MODEL_PATH = 'yolo11n.pt'  # Nano model = 30+ FPS on CPU
-CONFIDENCE = 0.7  # Lower for crash sensitivity
->>>>>>> master
 WINDOW_NAME = 'CRASH DETECTOR'
 CAMERA_ID = 2
